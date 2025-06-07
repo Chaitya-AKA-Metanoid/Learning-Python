5cb19aa3c5f420b125fe7508a316a823dfa00646
CHAITYA <chaityatests@gmail.com>
2025-06-07 14:42:01 +0530

    Day1 Add:practice.py - continued Python learning

---
+++ day1.py
@@ -0,0 +1,19 @@
+print("Day 1 of Relearning how to Code")
+
+name = "Chaitya"
+investment = 10000
+profit = 1000
+
+#conditions 
+if profit > 0 :
+    print("Good Going")
+else : 
+    print("pura thela laal hai")
+
+for i in range(5):
+    print("training round:", i+1)
+
+def greet(user):
+    return f"Hello {user}, welcome back."
+
+print(greet(name))
